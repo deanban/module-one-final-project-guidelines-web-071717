eb35bc136d04a3137b4b0b4e6c2d04638d8a77d5
linkcoding <kenny.lin.91@gmail.com>
2017-07-31 17:45:50 -0400

    create actors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731202746) do
+ActiveRecord::Schema.define(version: 20170731212607) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string  "director_name"
