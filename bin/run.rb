e3b6f0e7fa449b3ab0f0272ae2eba63350d3a548
linkcoding <kenny.lin.91@gmail.com>
2017-08-02 13:12:07 -0400

    added search by year(s) and case insensitive for initial input

--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,11 @@
 require_relative '../config/environment'
-
-
+ActiveRecord::Base.logger = nil 
 
 class MovieSearcher
 	include SearchFunctions
 
-	attr_accessor :film, :actor, :genre, :rating
-	
+	attr_accessor :film, :actor, :genre, :rating, :year1, :year2
+
 	def welcome
 		puts "What are you interested in?"
 		#puts "If you are interested in 10,000 B.C. Go F yourself!"
@@ -16,55 +15,37 @@ class MovieSearcher
 	def input_checker()
 		input = gets.chomp
 
-		case input 
-			when "Films"
-				puts "What film do you want to know about?"
-				films_info
-				
-			when "Actors"
-				puts "Any specific actor you want to know about?"
-				actor_info
-
-			when "Genre"
-				puts "Got any genre in mind?"
-				genre_info
-
-			# # when "Year" 
-			# # 	puts "Do you want to checkout a range of years, or a specific one?"
-			# # 	when 'Range'
-			# # 		#do this
-			# # 	else
-			# # 		films_by_year
-
-
-
-
-			# 	puts "Which year are you interested in?"
-			# 	films_by_year
-
-				
-
-			when 'exit'
-				gtfo	
-
-			else
-				puts "Pick an actual option!"
-				input_checker
+		if input.casecmp('films') == 0
+			puts "What film do you want to know about?"
+			films_info
+		elsif input.casecmp('actors') == 0
+			puts "Any specific actor you want to know about?"
+			actor_info
+		elsif input.casecmp('genre') == 0
+			puts "Got any genre in mind?"
+			genre_info
+		elsif input.casecmp('year') == 0
+			puts "Do you want to checkout a range of years, or a specific one?"
+			preference = gets.chomp
+			if preference == 'Range'
+				puts "Put in the starting year"
+				@year1 = gets.chomp
+				puts "Put in the ending year"
+				@year2 = gets.chomp
+				films_by_year_range
+			elsif preference == 'Specific'
+				films_by_year
 			end
-		# elsif input == "rating"
-			#actor_searcher
-		#  elsif input == "Year"
-		# 	films_by_year
-		# elsif input == "actors"
-		# 	Actor.where(name: "input")
+		elsif input.casecmp('exit') == 0
+			gtfo
+		else
+			puts "Pick an actual option!"
+			input_checker
+		end
 
 	end
 
-
-	# def display_films(films)
-
-	# end
-	def runner 
+	def runner
 		welcome
 		input_checker
 	end
@@ -83,4 +64,3 @@ MovieSearcher.new.runner
 #store vals as instance varaiable
 #get nested serchers setup
 #have a way to redo the search
-
--- lib/search_module.rb
+++ lib/search_module.rb
@@ -1,8 +1,18 @@
-module SearchFunctions	
+module SearchFunctions
 	def films_by_year
-		input = gets.chomp
-		@@film = Film.where('year: input')
-	
+		puts "Pick a year"
+		@year1 = gets.chomp
+		list = Film.where(year: @year1)
+		puts "These movies were released in the year #{@year1}:"
+		puts film_template(list)
+
+	end
+
+	def films_by_year_range
+		#range = "#{@year1}...#{@year2}"
+		list = Film.where(year: @year1...@year2)
+		puts "From #{@year1} to #{@year2} these movies were released:"
+		puts film_template(list)
 	end
 
 	def films_info
@@ -16,13 +26,28 @@ module SearchFunctions
 	def actor_info
 		input = gets.chomp
 		@actor = Actor.find_by(name: input)
-		puts "#{@actor.name} starred in #{@actor.films}"
+		puts "#{@actor.name} starred in #{film_template(@actor.films)}"
 	end
 
 	def genre_info
 		input = gets.chomp
 		@genre = Genre.find_by(name: input)
 		puts "There are #{@genre.films.count} films categorized as #{@genre.name} in our Database."
+	end
+
+	def film_template(arg)
+		film_parser = arg.all.map do |film|
+			film.title
+		end
+		#fixe these up, sometimes there are less than 3 films
+		if film_parser.count > 3
+			film_parser[-1] = "and " + film_parser[-1]
+			film_parser.join(", \n")
+		elsif film_parser.count == 2
+			film_parser = "#{film_parser[0]} and #{film_parser[1]}."
+		elsif film_parser.count == 1
+			film_parser.join
+		end
 	end
 
 	# def rating_info
@@ -32,18 +57,20 @@ module SearchFunctions
 	# # end
 
 	def actor_template(arg)
-		actor_parser = arg.actors.map do |actor| 
+		actor_parser = arg.actors.map do |actor|
 			actor.name
 		end
+		#fixe these up, sometimes there are less than 3 actors
 		actor_parser[-1] = "and " + actor_parser[-1]
 		actor_parser.join(", ")
 
 	end
 
 	def genre_template(arg)
-		genre_parser = arg.genres.map do |genre| 
+		genre_parser = arg.genres.map do |genre|
 			genre.name
 		end
+		#look at actors.
 		genre_parser[-1] = "and " + genre_parser[-1]
 		genre_parser.join(", ")
 	end
@@ -53,18 +80,18 @@ module SearchFunctions
 	def films_by_rating
 		input = gets.chomp
 		puts "****#{Film.find_by(title: input).rating}*****"
-	
+
 	end
 
 	def films_by_actor
 		input = gets.chomp
 		#puts "****#{Actor.find_by(name: input)}*****"
-	
+
 	end
 
 	def films_by_genre
 		input = gets.chomp
 		#puts "****#{Genre.find_by(title: input)}*****"
-	
+
 	end
 end
